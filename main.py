ff668ddb0049330bceba2b5982ce49def78a47c4
VadymCov <vadymkravtcsov@gmail.com>
2025-08-02 05:19:45 +0300

    added function create datadase

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from aiogram import Bot, Dispatcher
 
 
 from config import YOUR_TOKEN
+from database.models import init_db
+from handlers.callbacks import callback_router
+from handlers.messages import message_router
 
 from handlers.start import start_router
 
@@ -10,10 +13,11 @@ bot = Bot(YOUR_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(start_router)
-
-
+dp.include_router(message_router)
+dp.include_router(callback_router)
 
 async def main():
+    init_db()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
