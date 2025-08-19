31c37576eae82040e774f8b8c806388f4e0d4dea
VadymCov <vadymkravtcsov@gmail.com>
2025-08-19 22:08:57 +0300

    feat: created django 'todolist'app,  added app model, registered admin user

---
+++ mysite/todolist/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField("Task", max_length=200)
+    is_complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
