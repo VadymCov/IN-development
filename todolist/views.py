7997196bcfe5c86cc947b08f30fb2c92cc0a24d1
VadymCov <vadymkravtcsov@gmail.com>
2025-08-21 23:22:51 +0300

    feat: added @require_POST view for creating tasks

--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 from .models import Task
 # Create your views here.
 
 def index(request):
     task = Task.objects.all()
-    return render(request, 'index.html', {'task': task, 'title': 'Todo List'})
+    return render(request, 'todolist/index.html', {'task': task, 'title': 'Todo List'})
+
+@require_POST
+def add(request):
+    task_name = request.POST.get("task_name")
+    Task.objects.create(title = task_name)
+    return redirect('index')
