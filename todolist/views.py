1912397b9a2eee1c74b6f40f74f808031928f5d1
VadymCov <vadymkravtcsov@gmail.com>
2025-08-22 23:19:16 +0300

    feat: added new functionality, updates and task deletions

--- todolist/views.py
+++ todolist/views.py
@@ -5,11 +5,26 @@ from .models import Task
 
 def index(request):
     task = Task.objects.all()
-    return render(request, 'todolist/index.html', {'task': task, 'title': 'Todo List'})
+    print(f"{task.query}")
+
+    return render(request, 'todolist/index.html', {'tasks': task, 'title': 'Todo List'})
 
 @require_POST
 def add(request):
     task_name = request.POST.get("task_name")
-    if task_name:
-        Task.objects.create(title = task_name)
+    if len(task_name) > 5:
+        Task.objects.create(title=task_name)
     return redirect('index')
+
+def update(request, task_id):
+    task_name = Task.objects.get(id=task_id)
+    task_name.is_complete = not task_name.is_complete
+    task_name.save()
+    return redirect('index')
+
+def delete(request, task_id):
+    task_name = Task.objects.get(id=task_id)
+    task_name.delete()
+    return redirect('index')
+
+
