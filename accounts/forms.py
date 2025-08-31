69da3aed476d2c2fe98e5d217bfa880287d1234b
Vadym <vadymkravtcsov@gmail.com>
2025-08-31 22:31:50 +0300

    feat: add account registration form

---
+++ accounts/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=15, required=True)
+    last_name = forms.CharField(max_length=15, required=True)
+    
+    
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "password1",
+            "password2",
+            "email",
+            "first_name",
+            "last_name" )
+    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password1"].help_text = ""  
+        self.fields["password2"].help_text = ""  
+        self.fields["password1"].widget.attrs['placeholder'] = 'Password (min. 8 characters, letters & numbers)'
+        self.fields["password2"].widget.attrs['placeholder'] = 'Repeat password'
+        
+        self.fields["email"].widget.attrs['placeholder'] = 'you@example.com'
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        if commit:
+            user.save()
+        return user
