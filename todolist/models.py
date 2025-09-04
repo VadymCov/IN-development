b1b908be076a571e524c0545c1294d2912b0c3e3
Vadym <vadymkravtcsov@gmail.com>
2025-09-04 19:21:08 +0300

    feat: add field 'user, create_at, description' in model Task

--- todolist/models.py
+++ todolist/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField("Task", max_length=200)
+    description = models.TextField(blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
     is_complete = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.title
