7903f241d8820f42c531641d017f3de57b4775ee
Vadym <vadymkravtcsov@gmail.com>
2025-09-05 01:13:58 +0300

    feat: add a method for automatically saving a user to a profile databasehas been created

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import Count
+from django.db.models import Count, Q
 
 # Create your models here.
 
 class Profiles(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     avatar = models.ImageField(upload_to='avatar/', blank=True)
     telephone = models.CharField(max_length=20, blank=True)
     bio = models.TextField(max_length=500, blank=True)
@@ -25,9 +25,18 @@ class Profiles(models.Model):
     def task_stats(self):
         from todolist.models import Task
 
-        return Task.objects.filter(user=self.user).aggregate(
+        return Task.objects.filter(created_by=self.user).aggregate(
             total=Count('id'),
-            completed=Count('id', filter=models.Q(is_complete=True)),
-            not_completed=Count('id', filter=models.Q(is_complete=False))
+            completed=Count('id', filter=Q(is_complete=True)),
+            not_completed=Count('id', filter=Q(is_complete=False))
 
         )
+    
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profiles.objects.create(user=instance)
+        
