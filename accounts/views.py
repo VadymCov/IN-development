6145db222a95219ddff4bc8b3e7d882678c838a7
Vadym <vadymkravtcsov@gmail.com>
2025-09-06 00:07:22 +0300

    fix: add profile function added abillity to view multiple tasks

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.views import generic
+from django.urls import reverse_lazy
+from todolist.models import Task
+from .forms import RegisterForm
+from .models import Profiles
 # Create your views here.
+
+class RegisterView(generic.CreateView):
+    form_class = RegisterForm
+    template_name = 'accounts/registry.html'
+    success_url = reverse_lazy("accounts:login")
+
+@login_required 
+def profile(request):
+    profiles = Profiles.objects.all()
+    recent_tasks = Task.objects.filter(created_by=request.user).order_by('-create_at')[:3] 
+    
+    return render(request, 'accounts/profile.html', {
+        'profiles': profiles,
+        'title': 'Profiles',
+        'recent_tasks': recent_tasks
+        })
+
