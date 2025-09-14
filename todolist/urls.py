593293634fe143c5c51ed30b3243db9557805b38
Vadym <vadymkravtcsov@gmail.com>
2025-09-14 10:58:16 +0300

    feat: add URLs for task_add_form, add, update and delete views

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,6 +2,13 @@
 from django.urls import path
 from todolist import views
 
+app_name = 'todolist'
+
 urlpatterns = [
-    path('', views.index, name='index')
-]
+    path('', views.HomePageView.as_view(), name='home'),
+    path('index/', views.index, name='index'),
+    path('task_add_form/', views.task_add_form, name='task_add_form'),
+    path('add/', views.add, name='add'),
+    path('update/<int:task_id>/', views.update, name='update'),
+    path('delete/<int:task_id>/', views.delete, name='delete'),
+    ]
